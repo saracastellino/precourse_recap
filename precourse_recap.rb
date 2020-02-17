093a8979aa137dad9e134b028e5d78c9213e3820
saracastellino <sara.castellino0@gmail.com>
2020-02-17 16:04:42 +0000

    adds Git and GitHub homework

--- precourse_recap.rb
+++ precourse_recap.rb
@@ -1,1 +1,25 @@
-blah blah
+
+Conditional (if, else)
+coding_status = true
+if true
+  puts "I'm coding"
+else puts "Hem, I wasn't sleeping, I promise!"
+end
+
+Function
+def greeting
+  puts "Hello"
+end
+
+Array
+array = ["Hello", 1, 5.6]
+
+Hash
+fridge = {apples: 3, hummus: 1, ham: 2}
+
+Loop
+i = 0
+while i < 10
+  puts "I love Ruby"
+  i += 1
+end
